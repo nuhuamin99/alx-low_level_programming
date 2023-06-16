e8ca5c56e62e92a9034e822e1e9be1ed21557fa8
nuhuamin99 <nuhuamin99@gmail.com>
2023-06-15 19:12:41 -0700

    4-puts.c.rem1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+
 int main(void) 
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
